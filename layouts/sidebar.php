4b38f82a9d7e82e6417208b97dc5088bc0dca589
stitou-nada <nadastito1087@gmail.com>
2022-12-28 11:10:06 +0100

    les name de sidbare

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -35,7 +35,7 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="buttons.html">Taches</a>
+                        <a class="collapse-item" href="index">Taches</a>
                     </div>
                 </div>
             </li>
@@ -50,7 +50,7 @@
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="utilities-color.html">Tache</a>
+                        <a class="collapse-item" href="create">Tache</a>
                     </div>
                 </div>
             </li>
